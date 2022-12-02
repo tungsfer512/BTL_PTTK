0c1668a875b3886afd70b329d5d43ff558398d25
Dat Dang <datd61340@gmail.com>
2022-12-02 22:35:07 +0700

    them xu ly shop

--- src/main/java/io/team05/btl/routes/SellerController.java
+++ src/main/java/io/team05/btl/routes/SellerController.java
@@ -1,13 +1,26 @@
 package io.team05.btl.routes;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.web.bind.annotation.*;
 
 import io.team05.btl.controller.daoimpl.*;
+import io.team05.btl.model.Product;
+import io.team05.btl.model.Seller;
 
 @RestController
 @CrossOrigin
 public class SellerController {
     @Autowired
     SellerDAOImpl sellerDaoImpl;
+    @GetMapping("/api/shop/{shop_id}")
+    public Seller getAllSellerById(@PathVariable Integer id) {
+        return sellerDaoImpl.getSellerById(id);
+    }
+    
+    @GetMapping("/api/shop/{shop_id}/category")
+    public Seller getAllCategoryInShop(@PathVariable Integer id) {
+        return sellerDaoImpl.getSellerById(id);
+    }
 }
